c9c61ae872167c365c17b57a3cbe6f7983a7d754
AmasMovsisian <232450988+AmasMovsisian@users.noreply.github.com>
2025-12-03 14:43:05 +0100

    The start page is now updated, designed, and functional!

--- js/game.js
+++ js/game.js
@@ -7,47 +7,69 @@ function init() {
     world = new World(canvas, keyboard);
 }
 
-window.addEventListener('keydown', (e) => {
-    if (e.code == "ArrowRight") {
-        keyboard.RIGHT = true;
-    }
-    if (e.code == "ArrowLeft") {
-        keyboard.LEFT = true;
-    }
-    if (e.code == "ArrowUp") {
-        keyboard.UP = true;
-    }
-    if (e.code == "ArrowDown") {
-        keyboard.DOWN = true;
-    }
-    if (e.code == "Space") {
-        keyboard.SPACE = true;
-    }
 
-    if (e.code == "KeyD") {
-        keyboard.D = true;
+function startGame() {
+    const menu = document.getElementById('menu');
+    const canvas = document.getElementById('canvas');
+
+    if (menu && canvas) {
+        menu.style.display = 'none';
+        canvas.style.display = 'block';
+
+
+        init();
+    } else {
+        console.error('Menu or canvas element not found');
     }
+}
+
+
+document.addEventListener('DOMContentLoaded', function () {
+    window.addEventListener('keydown', (e) => {
+        if (e.code == "ArrowRight") {
+            keyboard.RIGHT = true;
+        }
+        if (e.code == "ArrowLeft") {
+            keyboard.LEFT = true;
+        }
+        if (e.code == "ArrowUp") {
+            keyboard.UP = true;
+        }
+        if (e.code == "ArrowDown") {
+            keyboard.DOWN = true;
+        }
+        if (e.code == "Space") {
+            keyboard.SPACE = true;
+        }
+        if (e.code == "KeyD") {
+            keyboard.D = true;
+        }
+    });
+
+    window.addEventListener('keyup', (e) => {
+        if (e.code == "ArrowRight") {
+            keyboard.RIGHT = false;
+        }
+        if (e.code == "ArrowLeft") {
+            keyboard.LEFT = false;
+        }
+        if (e.code == "ArrowUp") {
+            keyboard.UP = false;
+        }
+        if (e.code == "ArrowDown") {
+            keyboard.DOWN = false;
+        }
+        if (e.code == "Space") {
+            keyboard.SPACE = false;
+        }
+        if (e.code == "KeyD") {
+            keyboard.D = false;
+        }
+    });
 });
 
-window.addEventListener('keyup', (e) => {
-    if (e.code == "ArrowRight") {
-        keyboard.RIGHT = false;
-    }
-    if (e.code == "ArrowLeft") {
-        keyboard.LEFT = false;
-    }
-    if (e.code == "ArrowUp") {
-        keyboard.UP = false;
-    }
-    if (e.code == "ArrowDown") {
-        keyboard.DOWN = false;
-    }
-    if (e.code == "Space") {
-        keyboard.SPACE = false;
-    }
 
-    if (e.code == "KeyD") {
-        keyboard.D = false;
-    }
-});
-
+const startButton = document.getElementById('startButton');
+if (startButton) {
+    startButton.addEventListener('click', startGame);
+}
